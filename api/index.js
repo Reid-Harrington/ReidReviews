a33a545940a0045217b1475771b5459cb0084e9f
Reid-Harrington <128394267+Reid-Harrington@users.noreply.github.com>
2023-08-15 19:16:55 -0400

    Add files via upload
    changed backend call

--- api/index.js
+++ api/index.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json())
 app.use(
     cors({
-      origin: "http://localhost:5173", // Replace with the correct URL of your frontend
+      origin: "https://rrbackend.onrender.com", // Replace with the correct URL of your frontend
       credentials: true, // Enable credentials (cookies, authorization headers, etc.)
     })
   );
